e09cc7e9f82c8b05da609b1eb3815a0cac96fef0
Mike Creque <mike.creque@block.one>
2018-12-03 15:11:55 -0500

    publish only specific branch

--- scripts/is_latest.sh
+++ scripts/is_latest.sh
@@ -10,9 +10,9 @@ latest_tag="${arry[0]}"
 if [ "$latest_tag" == "" ]; then 
     latest_tag="v0.0.0";
 else
-    git branch --contains tags/${latest_tag}
+    tagged_branch="$(git branch --contains tags/${latest_tag} | grep -E "\b${1}$" | awk '{ print $1}')"
     tag_commit="$(git rev-list -n 1 ${latest_tag})";
-    if [ "$tag_commit" = "$current_commit" ]; then
+    if [ "$tag_commit" == "$current_commit" ]  && [ "$tagged_branch" == "$1" ]; then
         is_latest=true;
     fi
 fi
@@ -20,5 +20,6 @@ fi
 echo "tag_commit: ${tag_commit}";
 echo "current_commit: ${current_commit}";
 echo "is_latest: ${is_latest}";
+echo "tagged_branch: ${tagged_branch}";
 
 export TRAVIS_IS_LATEST_TAG="$is_latest"
