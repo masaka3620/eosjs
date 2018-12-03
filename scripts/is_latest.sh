4c2541266a2de329e5ed9870e00a83766c5ba1dc
Mike Creque <mike.creque@block.one>
2018-12-03 16:57:08 -0500

    can't check branches for tags

--- scripts/is_latest.sh
+++ scripts/is_latest.sh
@@ -6,14 +6,12 @@ tags="$(git tag --sort=-creatordate)";
 IFS='\n' read -ra arry <<< "$tags"
 
 latest_tag="${arry[0]}"
-tagged_branch=""
 
 if [ "$latest_tag" == "" ]; then 
     latest_tag="v0.0.0";
 else
-    tagged_branch="$(git branch --contains tags/${latest_tag} | grep -E "\b${1}$" | awk '{ print $1}')"
     tag_commit="$(git rev-list -n 1 ${latest_tag})";
-    if [ "$tag_commit" == "$current_commit" ]  && [ "$tagged_branch" == "$1" ]; then
+    if [ "$tag_commit" == "$current_commit" ]; then
         is_latest=true;
     fi
 fi
@@ -21,6 +19,5 @@ fi
 echo "tag_commit: ${tag_commit}";
 echo "current_commit: ${current_commit}";
 echo "is_latest: ${is_latest}";
-echo "tagged_branch: ${tagged_branch}";
 
 export TRAVIS_IS_LATEST_TAG="$is_latest"
